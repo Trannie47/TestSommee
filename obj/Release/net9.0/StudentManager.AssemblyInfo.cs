d619c8041e03e4bf8b449f1029315bd142706f84
Trannie47 <tranghuyen2012003@gmail.com>
2025-12-14 15:15:52 +0700

    build

--- obj/Release/net9.0/StudentManager.AssemblyInfo.cs
+++ obj/Release/net9.0/StudentManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+542f63fb99327ac5e9da7ed478b22e6cc43ea65a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdbe2627b2df7027546d0158a88bb253aed7de97")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
